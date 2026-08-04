9d51cf2c2423bcaa2ee4a5e7749994ad1d3e896d
Nodo1221 <killforskill2005@gmail.com>
2026-08-04 05:24:29 +0200

    Migrate to clap, refactor old code

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,18 @@ use rustls::pki_types::{PrivateKeyDer};
 use std::sync::Arc;
 use std::io::{IsTerminal, Read, Write};
 use std::net::{IpAddr, TcpListener, UdpSocket};
+use clap::{Command, CommandFactory, Parser};
 
 const DEFAULT_PORT: u16 = 4000;
 
-fn local_ip() -> std::io::Result<IpAddr> {
-    let socket = UdpSocket::bind("0.0.0.0:0")?;
-    socket.connect("192.0.2.1:80")?;
-    Ok(socket.local_addr()?.ip())
+fn local_ip() -> IpAddr {
+    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    socket.connect("192.0.2.1:80").unwrap();
+    socket.local_addr().unwrap().ip()
 }
 
-fn usage() -> ! {
-    eprintln!("usage: share [-ke] [-p port] [file | -]");
+fn usage(mut args: Command) -> ! {
+    args.print_help().unwrap();
     std::process::exit(1);
 }
 
@@ -107,30 +108,29 @@ fn handle_connection(
     Ok(matches!(request, Request::Range(..)))
 }
 
+#[derive(Parser)]
+struct Args {
+    #[arg(short = 't')]
+    tls: bool,
+
+    #[arg(short = 'k')]
+    keep_open: bool,
+
+    #[arg(short = 'e')]
+    embed_video: bool,
+
+    #[arg(short = 'p', default_value_t = DEFAULT_PORT)]
+    port: u16,
+
+    file_path: Option<String>,
+}
+
 fn main() -> std::io::Result<()> {
-    let mut pargs = pico_args::Arguments::from_env();
-    let keep_open = pargs.contains(["-k", "--keep-open"]);
-    let embed_video = pargs.contains(["-e", "--embed-video"]);
-    let port: u16 = pargs
-        .opt_value_from_str(["-p", "--port"])
-        .unwrap_or_else(|_| usage())
-        .unwrap_or(DEFAULT_PORT);
-    let cert: Option<String> = pargs
-        .opt_value_from_str(["-c", "--cert"])
-        .unwrap_or_else(|_| usage());
-    let key: Option<String> = pargs
-        .opt_value_from_str(["-K", "--key"])
-        .unwrap_or_else(|_| usage());
-    let file_path: Option<String> = pargs.opt_free_from_str().unwrap_or_else(|_| usage());
+    let cmd = Args::command();
+    let args = Args::parse();
 
-    let tls = match (cert, key) {
-        (Some(c), Some(k)) => Some(make_tls_config(&c, &k)?),
-        (None, None) => None,
-        _ => usage(),
-    };
-
-    let body = match file_path.as_deref().unwrap_or("-") {
-        "-" if std::io::stdin().is_terminal() => usage(),
+    let body = match args.file_path.as_deref().unwrap_or("-") {
+        "-" if std::io::stdin().is_terminal() => usage(cmd),
         "-" => {
             let mut buf = Vec::new();
             std::io::stdin().read_to_end(&mut buf)?;
@@ -139,16 +139,21 @@ fn main() -> std::io::Result<()> {
         path => std::fs::read(path)?,
     };
 
+    let tls = match args.tls {
+        true => Some(make_tls_config("cert.pem", "key.pem")),
+        false => None,
+    };
+
     let (size, unit) = match body.len() as f32 {
-        0.0 => usage(),
+        0.0 => usage(cmd),
         s @ ..1_048_576.0 => (s / 1024.0, "KiB"),
         s => (s / 1024.0 / 1024.0, "MiB"),
     };
 
-    let listener = TcpListener::bind(("0.0.0.0", port))?;
+    let listener = TcpListener::bind(("0.0.0.0", args.port))?;
     let mut buf = [0u8; 4096];
 
-    println!("Sharing [{size:.1}{unit}] @ http://{}:{port}", local_ip()?);
+    println!("Sharing [{size:.1}{unit}] @ http://{}:{}", args.port, local_ip());
 
     for stream in listener.incoming() {
         let stream = stream?;
@@ -157,11 +162,11 @@ fn main() -> std::io::Result<()> {
         let res = match &tls {
             Some(cfg) => {
                 match ServerConnection::new(cfg.clone()) {
-                    Ok(conn) => handle_connection(StreamOwned::new(conn, stream), &mut buf, &body, embed_video),
+                    Ok(conn) => handle_connection(StreamOwned::new(conn, stream), &mut buf, &body, args.embed_video),
                     Err(e) => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, e)),
                 }
             }
-            None => handle_connection(stream, &mut buf, &body, embed_video),
+            None => handle_connection(stream, &mut buf, &body, args.embed_video),
         };
 
         let is_range = match res {
@@ -172,7 +177,7 @@ fn main() -> std::io::Result<()> {
             }
         };
 
-        if !keep_open && !is_range {
+        if !args.keep_open && !is_range {
             break;
         }
     }
